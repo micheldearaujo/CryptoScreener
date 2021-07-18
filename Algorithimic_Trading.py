8843e87214c5de39b7e1fa9149eb5dfe490901f9
Michel de Araujo <78417934+micheldearaujo@users.noreply.github.com>
2021-07-17 21:58:39 -0300

    I have created a simple cryptocurrency scraper

--- Algorithimic_Trading.py
+++ Algorithimic_Trading.py
@@ -22,7 +22,8 @@ from base64 import b64encode
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
-import sys, os
+import sys
+import os
 
 plt.style.use('dark_background')
 
@@ -38,189 +39,205 @@ end = str(dt.datetime.now().date()) # Ending date (now)
 # Let's get the data through the Pandas DataReader and the Yahoo Finance API.
 company = 'TSLA'
 
-# Creating a email configuration
-def config_email(text,image_name=None):
-    with open(image_name, 'rb') as f:
-        img_data = f.read()
 
-    msg = MIMEMultipart()
-    keys = open('D:/Projects/emails.txt', 'r').read().splitlines()
-    msg['Subject'] = 'Relatório Diário'
-    msg['From'] = keys[0]
-    msg['To'] = 'Destination email here'
+class Trader:
 
-    text = MIMEText(f'{text}')
-    msg.attach(text)
-    image = MIMEImage(img_data, name=os.path.basename(image_name))
-    msg.attach(image)
-    return msg
+    buy_signals = []  # Making a list of the buying signals
+    sell_signals = []
+    trigger = 0  # This trigger is used to verify the current state of the algorithm
 
-def send_email(msg):
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.ehlo()
-    s.starttls()
-    s.ehlo()
+    def __init__(self, company_name):
+        self.company_name = company_name
 
-    keys = open('./data/emails.txt', 'r').read().splitlines()
-    s.login(keys[0],
-            keys[1])
+    # Creating a email configuration
+    @staticmethod
+    def config_email(self, text, image_name=None):
+        with open(image_name, 'rb') as f:
+            img_data = f.read()
 
-    s.sendmail(msg['From'],
-               msg['To'], msg.as_string())
-    s.quit()
+        msg = MIMEMultipart()
+        keys = open('D:/Projects/emails.txt', 'r').read().splitlines()
+        msg['Subject'] = 'Relatório Diário'
+        msg['From'] = keys[0]
+        msg['To'] = 'Destination email here'
 
-def get_data(company_name, start_date, end_date):
-    #data = web.DataReader(company, 'yahoo', start, end)
-    data = yf.Ticker(company_name).history(start=start_date, end=end_date) # Getting the data from the yfinance API
-    print(data)
+        text = MIMEText(f'{text}')
+        msg.attach(text)
+        image = MIMEImage(img_data, name=os.path.basename(image_name))
+        msg.attach(image)
+        return msg
 
+    @staticmethod
+    def send_email(self, msg):
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
 
-    # Calculate the moving averages
-    data[f'SMA_{ma_1}'] = data['Close'].rolling(window=ma_1).mean()
-    data[f'SMA_{ma_2}'] = data['Close'].rolling(window=ma_2).mean()
+        keys = open('./data/emails.txt', 'r').read().splitlines()
+        s.login(keys[0],
+                keys[1])
 
-    # Filtering the data to start in at the bigger moving average days, because before that there are no values
-    data = data.iloc[ma_2:]
+        s.sendmail(msg['From'],
+                   msg['To'], msg.as_string())
+        s.quit()
 
-    data.to_csv(f'./data/{company}.csv')
-    return(data)
+    def get_data(self, company_name, start_date, end_date, ma_1, ma_2):
+        # data = web.DataReader(company, 'yahoo', start, end)
+        self.data = yf.Ticker(company_name).history(start=start_date, end=end_date) # Getting the data from the yfinance API
+        print(self.data)
 
-def get_data_off(company):
-    data = pd.read_csv(f'./data/{company}.csv')
-    return data
-data = get_data_off(company)
-#data = get_data(company, start, end)
+        # Calculate the moving averages
+        self.data[f'SMA_{ma_1}'] = self.data['Close'].rolling(window=ma_1).mean()
+        self.data[f'SMA_{ma_2}'] = self.data['Close'].rolling(window=ma_2).mean()
 
-# Plotting the values
-def plot_initial(data):
-    plt.plot(data['Close'], label='Share Price', color='lightgray')
-    plt.plot(data[f'SMA_{ma_1}'], label=f'SMA_{ma_1}', color='orange')
-    plt.plot(data[f'SMA_{ma_2}'], label=f'SMA_{ma_2}', color='purple')
-    plt.title(f'Trading Strategy for {company} Stocks')
-    plt.legend(loc='upper left')
-    plt.show()
+        # Filtering the data to start in at the bigger moving average days, because before that there are no values
+        self.data = self.data.iloc[ma_2:]
+        #self.data.reset_index(inplace=True)
+        self.data.to_csv(f'./data/{self.company_name}.csv')
+        return self.data
 
-# Ok, that the first part: Gathering the data, defining moving averages and making some chats about it.
-# Now we gotta move to the next level: Create a trading strategy to buy and sell at the perfect time.
-# This strategy is based on the crossing of the moving averages. When they cross, we are going to buy or sell
+    def get_data_off(self):
+        self.data = pd.read_csv(f'./data/{self.company_name}.csv')
+        return self.data
 
-#def execute_trading(data):
-buy_signals = []        # Making a list of the buying signals
-sell_signals = []
-trigger = 0             # This trigger is used to verify the current state of the algorithm
+    # Plotting the values
+    def plot_initial(self):
+        plt.plot(self.data['Close'], label='Share Price', color='lightgray')
+        plt.plot(self.data[f'SMA_{ma_1}'], label=f'SMA_{ma_1}', color='orange')
+        plt.plot(self.data[f'SMA_{ma_2}'], label=f'SMA_{ma_2}', color='purple')
+        plt.title(f'Trading Strategy for {self.company_name} Stocks')
+        plt.legend(loc='upper left')
+        plt.show()
 
-# Setting the strategy:
-# We will iterate over the dataframe rows and compare the moving averages prices
-for x in range(len(data)):
-    # If the minor moving average cross up the bigger moving average we will buy the stock
-    # Because hypothetically it means that the stock price will increase after that
-    if data[f'SMA_{ma_1}'].iloc[x] > data[f'SMA_{ma_2}'].iloc[x] and trigger !=1:
-        buy_signals.append(data['Close'].iloc[x])
-        sell_signals.append(float('nan'))
-        trigger = 1
+    # Ok, that the first part: Gathering the data, defining moving averages and making some chats about it.
+    # Now we gotta move to the next level: Create a trading strategy to buy and sell at the perfect time.
+    # This strategy is based on the crossing of the moving averages. When they cross, we are going to buy or sell
 
-        #msg = config_email(text=f"Opa meu amigo! Estou aqui olhando as ações da {company} e na data {data.index[x]} você deve"
-         #                  f"vender todas as suas ações, cada uma por {data['Close'].iloc[x]}")
-        #send_email(msg)
+    def execute_trading(self, ma_1, ma_2):
 
-        # That will happen only if the trigger is not equal to one. Equal to 1 means that the last signal was a 'buy'
-        # And we don't want to perform the same action twice.
+        # Setting the strategy:
+        # We will iterate over the dataframe rows and compare the moving averages prices
+        for x in range(len(self.data)):
 
-    # If the smaller moving average crosses down the bigger one, we want to sell the stock
-    # because the price will fall after that crossing. And again
-    # We are only going to perform this action if the trigger is not equal to -1, the sell signal.
-    elif data[f'SMA_{ma_1}'].iloc[x] < data[f'SMA_{ma_2}'].iloc[x] and trigger !=-1:
-        buy_signals.append(float('nan'))
-        sell_signals.append(data['Close'].iloc[x])
-        trigger = -1
+            # If the minor moving average cross up the bigger moving average we will buy the stock
+            # Because hypothetically it means that the stock price will increase after that
+            if self.data[f'SMA_{ma_1}'].iloc[x] > self.data[f'SMA_{ma_2}'].iloc[x] and self.trigger !=1:
+                self.buy_signals.append(data['Close'].iloc[x])
+                self.sell_signals.append(float('nan'))
+                self.trigger = 1
 
-        #msg = config_email(text=f"Opa meu amigo! Estou aqui olhando as ações da {company} e na data {data.index[x]} você deve"
-         #                  f"comprar ações, cada uma por {data['Close'].iloc[x]}")
-        #send_email(msg)
+                # msg = config_email(text=f"Opa meu amigo! Estou aqui olhando as ações da {company} e na data {data.index[x]} você deve"
+                #                  f"vender todas as suas ações, cada uma por {data['Close'].iloc[x]}")
+                # send_email(msg)
 
-    # If nothing happens, append a 'nan' to the row.
-    else:
-        buy_signals.append(float('nan'))
-        sell_signals.append(float('nan'))
+                # That will happen only if the trigger is not equal to one. Equal to 1 means that the last signal was a 'buy'
+                # And we don't want to perform the same action twice.
 
-data['Buy Signals'] = buy_signals
-data['Sell Signals'] = sell_signals
-#return data
-#data = execute_trading(data)
+            # If the smaller moving average crosses down the bigger one, we want to sell the stock
+            # because the price will fall after that crossing. And again
+            # We are only going to perform this action if the trigger is not equal to -1, the sell signal.
+            elif self.data[f'SMA_{ma_1}'].iloc[x] < self.data[f'SMA_{ma_2}'].iloc[x] and self.trigger != -1:
+                self.buy_signals.append(float('nan'))
+                self.sell_signals.append(data['Close'].iloc[x])
+                self.trigger = -1
 
-print(data)
+                # msg = config_email(text=f"Opa meu amigo! Estou aqui olhando as ações da {company} e na data {data.index[x]} você deve"
+                #                  f"comprar ações, cada uma por {data['Close'].iloc[x]}")
+                # send_email(msg)
 
-# plt.plot(data['Close'], label='Share Price', alpha=0.5)
-# plt.plot(data[f'SMA_{ma_1}'], label=f'SMA_{ma_1}', color='orange', ls='--')
-# plt.plot(data[f'SMA_{ma_2}'], label=f'SMA_{ma_2}', color='purple', ls ='--')
-# plt.scatter(data.index, data['Buy Signals'], label='Buy Signals', marker='^', color='#00ff00', lw=3)
-# plt.scatter(data.index, data['Sell Signals'], label='Sell Signals', marker='v', color='#ff0000', lw=3)
-# plt.title(f'Trading Strategy for {company} Stocks')
-# plt.legend(loc='upper left')
-# plt.grid(alpha=0.2)
-# plt.show()
+            # If nothing happens, append a 'nan' to the row.
+            else:
+                self.buy_signals.append(float('nan'))
+                self.sell_signals.append(float('nan'))
 
-# The tutorial ends here. Now lets implement some other things to enhance this algorithm and measure how well it is going.
+        self.data['Buy Signals'] = self.buy_signals
+        self.data['Sell Signals'] = self.sell_signals
 
+        print(self.data)
 
-# Measuring How well the algorithm is going - Calculating the profits
-# Ok, lets suppose we have 1 Share of the Stock and we are trading it. We need to calculate
-# The profit ratio under the Share initial price.
-# For That we have to subtract the "Sell Signal" column of the "Buy Signal" Column.
+        # plt.plot(data['Close'], label='Share Price', alpha=0.5)
+        # plt.plot(data[f'SMA_{ma_1}'], label=f'SMA_{ma_1}', color='orange', ls='--')
+        # plt.plot(data[f'SMA_{ma_2}'], label=f'SMA_{ma_2}', color='purple', ls ='--')
+        # plt.scatter(data.index, data['Buy Signals'], label='Buy Signals', marker='^', color='#00ff00', lw=3)
+        # plt.scatter(data.index, data['Sell Signals'], label='Sell Signals', marker='v', color='#ff0000', lw=3)
+        # plt.title(f'Trading Strategy for {company} Stocks')
+        # plt.legend(loc='upper left')
+        # plt.grid(alpha=0.2)
+        # plt.show()
 
-TotalBought = data['Buy Signals'].sum()
-TotalSold = data['Sell Signals'].sum()
-TotalProfit = TotalSold - TotalBought
-print(TotalProfit)
+        # The tutorial ends here. Now lets implement some other things to enhance this algorithm and measure how well it is going.
 
-# Well, a negative value. That is because we still have the share and have to sell it to calculate de profit.
-# For now, let's just use the last Close Price of the dataframe as the "actual selling price" to calculate
-# How much profit we would have if we sell that share right now, and the first price of the stocks is the base
-# to calculate de percentage of the profit over the initial value.
+        return self.data
 
-InitialPrice = data['Close'].iloc[0]
+    def evaluate_model(self):
+        #  --------- Measuring How well the algorithm is going - Calculating the profits
+        # Ok, lets suppose we have 1 Share of the Stock and we are trading it. We need to calculate
+        # The profit ratio under the Share initial price.
+        # For That we have to subtract the "Sell Signal" column of the "Buy Signal" Column.
 
-# It its necessary to check the trigger. If the last action was a buying action, we need to set the final price
-# as the last price in the data.
-# But if the last action was a selling action, then we do not need to modify the sum.
+        TotalBought = self.data['Buy Signals'].sum()
+        TotalSold = self.data['Sell Signals'].sum()
+        TotalProfit = TotalSold - TotalBought
 
-if trigger==1: # Bought
-    FinalPrice = data['Close'].iloc[len(data) -1]
-    TotalProfit = round(TotalSold - TotalBought + FinalPrice, 2)
-    PercentageProfit = round(TotalProfit / InitialPrice * 100, 1)
-else:   # Sold
-    TotalProfit = round(TotalSold - TotalBought)
-    PercentageProfit = round(TotalProfit / InitialPrice * 100, 1)
-# So:
+        # Well, a negative value. That is because we still have the share and have to sell it to calculate de profit.
+        # For now, let's just use the last Close Price of the dataframe as the "actual selling price" to calculate
+        # How much profit we would have if we sell that share right now, and the first price of the stocks is the base
+        # to calculate de percentage of the profit over the initial value.
 
+        InitialPrice = self.data['Close'].iloc[0]
 
-# Plotting the final result
-fig, ax = plt.subplots(figsize=(15,10))
+        # It its necessary to check the trigger. If the last action was a buying action, we need to set the final price
+        # as the last price in the data.
+        # But if the last action was a selling action, then we do not need to modify the sum.
 
-ax.plot(data['Close'], label='Share Price', alpha=0.5)
-ax.plot(data[f'SMA_{ma_1}'], label=f'SMA_{ma_1}', color='orange', ls='--')
-ax.plot(data[f'SMA_{ma_2}'], label=f'SMA_{ma_2}', color='purple', ls ='--')
-ax.scatter(data.index, data['Buy Signals'], label='Buy Signals', marker='^', color='#00ff00', lw=3)
-ax.scatter(data.index, data['Sell Signals'], label='Sell Signals', marker='v', color='#ff0000', lw=3)
-plt.text(x=data.index.mean(),
-         y=data['Close'].max()*.98,
-         s=f'Total Profit per Share: U${TotalProfit} \nPercentage Profit: {PercentageProfit}%')
-ax.set_title(f'Trading Strategy for {company} Stocks', fontsize='18')
-ax.set_xlabel('Date')
-ax.set_ylabel('Price (US$')
+        if self.trigger == 1:  # Bought
+            FinalPrice = self.data['Close'].iloc[len(self.data) -1]
+            TotalProfit = round(TotalSold - TotalBought + FinalPrice, 2)
+            PercentageProfit = round(TotalProfit * 100, 1)
+        else:   # Sold
+            TotalProfit = round(TotalSold - TotalBought)
+            PercentageProfit = round(TotalProfit / InitialPrice * 100, 1)
+        # So:
 
-plt.legend(loc='upper left')
-plt.grid(alpha=0.2)
-plt.show()
-image_name = 'stocks.png'
-fig.savefig('data/' + image_name)
+        print(f'Your total profit was: {TotalProfit} USD\ntogether with the percentege profit: {PercentageProfit}%. \nCongratulations!')
+
+        # Plotting the final result
+        # fig, ax = plt.subplots(figsize=(15, 10))
+        # ax.plot(self.data['Close'], label='Share Price', alpha=0.5)
+        # ax.plot(self.data[f'SMA_{ma_1}'], label=f'SMA_{ma_1}', color='orange', ls='--')
+        # ax.plot(self.data[f'SMA_{ma_2}'], label=f'SMA_{ma_2}', color='purple', ls ='--')
+        # ax.scatter(self.data.index, data['Buy Signals'], label='Buy Signals', marker='^', color='#00ff00', lw=3)
+        # ax.scatter(self.data.index, data['Sell Signals'], label='Sell Signals', marker='v', color='#ff0000', lw=3)
+        # # plt.text(x=self.data['Date'].mean(),
+        # #          y=self.data['Close'].max()*.98,
+        # #          s=f'Total Profit per Share: U${TotalProfit} \nPercentage Profit: {PercentageProfit}%')
+        # ax.set_title(f'Trading Strategy for {company} Stocks \nTotal Profit per Share: U${TotalProfit} \nPercentage Profit: {PercentageProfit}%', fontsize='18')
+        # ax.set_xlabel('Date')
+        # ax.set_ylabel(f'Price (US$) || MA {ma_1, ma_2}')
+        #
+        # plt.legend(loc='upper left')
+        # plt.grid(alpha=0.2)
+        # plt.show()
+        # image_name = f'{self.company_name}.png'
+        # fig.savefig('data/' + image_name)
+
+        return company, TotalProfit, PercentageProfit
 
 #msg = config_email(image_name=image_name, text=f"Opa meu amigo! Terminamos aqui a análise do período indicado e temos os seguintes resultados.\n"
- #                       f"'Total Profit per Share: U${TotalProfit} \n"
-  #                      f"Percentage Profit: {PercentageProfit}%")
+#                       f"'Total Profit per Share: U${TotalProfit} \n"
+#                      f"Percentage Profit: {PercentageProfit}%")
 #send_email(msg)
 
 
+trader = Trader(company)
+data = trader.get_data(company, start, end, ma_1, ma_2)
+# data = trader.get_data_off()
+new_data = trader.execute_trading(ma_1, ma_2)
+trader.evaluate_model()
+
+
+#data = get_data(company, start, end)
 
 ## OK, now we now how much profit we got!
 
@@ -228,3 +245,17 @@ fig.savefig('data/' + image_name)
 # We gotta test for a bunch of combinations to see what combination performs better.
 ma_1 = [7, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210]
 ma_2 = [7, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210]
+profits = list()
+Evaluation = pd.DataFrame()
+for j in range(len(ma_1)):
+    for k in range(len(ma_2)):
+        if ma_1[j] == ma_2[k]:
+            pass
+        else:
+            trader = Trader(company)
+            data = trader.get_data(company, start, end, ma_1[j], ma_2[k])
+            data = trader.execute_trading(ma_1[j], ma_2[k])
+            Company, TotalProfit, PercentageProfit = trader.evaluate_model()
+            profits.append(f'{ma_1[j]}, {ma_2[k]}: {TotalProfit}, {PercentageProfit}')
+
+            print(f'{ma_1[j]} and {ma_2[k]}')
---
+++ Stocks_Monitor.py
@@ -0,0 +1,68 @@
+"""
+In this part of the project I intend to create a script to get streaming data from Yahoo Finance website
+And make some decisions about it (Maybe daytrade)
+
+This strategy works as follows: We're going to calculate 2 moving averages and when this averages cross
+We will sell or buy the stock.
+
+@micheldearaujo
+"""
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+import datetime as dt
+import csv
+import pandas as pd
+
+path = 'C:\Program Files (x86)\chromedriver.exe'
+url = 'https://finance.yahoo.com/cryptocurrencies'
+
+
+def create_csv(file_name):
+    f = open(file_name, 'w')
+    writer = csv.writer(f)
+    writer.writerow(('Date', 'Symbol', 'Name', 'Price', 'Change'))
+    f.close()
+
+
+def get_data(period, symbols, pos):
+
+    driver = webdriver.Chrome(path)
+    driver.get(url)
+    sleep(4)
+
+    while True:
+
+        counter = 0
+        for j in pos:
+
+            date = dt.datetime.now()
+            symbol = driver.find_element_by_xpath(f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{j}]/td[1]/a').text
+            name = driver.find_element_by_xpath(f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{j}]/td[2]').text
+            price = driver.find_element_by_xpath(f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{j}]/td[3]/span').text
+            change = driver.find_element_by_xpath(f'//*[@id="scr-res-table"]/div[1]/table/tbody/tr[{j}]/td[5]/span').text
+
+            f = open(f'./data/{symbols[counter]}.csv', 'a')
+            writer = csv.writer(f)
+            writer.writerow((date, symbol, name, price, change))
+            f.close()
+
+            stocks = pd.read_csv(f'./data/{symbols[counter]}.csv')
+            print(stocks)
+
+            counter += 1
+            print(counter)
+        sleep(period)
+
+file_name = 'BTC'
+period = 60
+symbols = ['BTC', 'ETH', 'ADA', 'DOGE', 'LTC']
+pos = [1, 2, 5, 9, 13]
+# Creating a new csv
+# for symbol in symbols:
+#     create_csv(f'./data/{symbol}.csv')
+
+# Executing the loop
+get_data(period, symbols, pos)
